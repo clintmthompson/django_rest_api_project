3fb51f96e907757aec7d116a29a940bb0c2f613f
Mega Farts <clint.m.thompson@outlook.com>
2021-06-11 12:27:24 -0500

    Created and registered music_app

--- music_project/music_project/settings.py
+++ music_project/music_project/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'music_app.apps.MusicAppConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
