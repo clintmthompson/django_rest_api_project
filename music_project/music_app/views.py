ecc0445272dbc1d41f5a2a41033b4ef41a75eaa5
Mega Farts <clint.m.thompson@outlook.com>
2021-06-14 10:51:54 -0500

    Created detail view in music_app

--- music_project/music_app/views.py
+++ music_project/music_app/views.py
@@ -12,7 +12,7 @@ class SongList(APIView):
     def get(self, request):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = SongSerializer(data=request.data)
@@ -20,3 +20,30 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SongDetail(APIView):
+
+    def get_song(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        Song = self.get_song(pk)
+        serializer = SongSerializer(Song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        Song = self.get_song(pk)
+        serializer = SongSerializer(Song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        Song = self.get_song(pk)
+        Song.delete()
+        return Response(status=status.HTTP_200_OK)
